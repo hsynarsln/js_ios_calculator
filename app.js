152b3809f3a43a52bebff215f89e7433921ef1b2
Hüseyin Arslan <hsyn06arslan@gmail.com>
2021-10-26 20:40:35 +0300

    initial

---
+++ app.js
@@ -0,0 +1,24 @@
+const result = document.querySelector('#display');
+const ac = document.querySelector('#ac');
+const plusMinus = document.querySelector('#plusMinus');
+const percentage = document.querySelector('#percentage');
+const quotient = document.querySelector('#quotient');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const multiplication = document.querySelector('#multiplication');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const subtraction = document.querySelector('#subtraction');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const addition = document.querySelector('#addition');
+const zero = document.querySelector('#zero');
+const decimal = document.querySelector('#decimal');
+const equal = document.querySelector('#equal');
+
+const numbers = [zero, one, two, three, four, five, six, seven, eight, nine];
+
+
